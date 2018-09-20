aa52c5e3c475083a066236748c7ab3d1577d0b63
= <abaez@esri.co>
2018-09-20 12:07:57 -0500

    R-Raster-Processing.R

--- R-Raster-Processing.R
+++ R-Raster-Processing.R
@@ -29,14 +29,17 @@ library(sp)
 library(raster)
 library(arcgisbinding)
 library(cluster)
-library("r")
+
 
 #verificando la licencia de ArcGIS
 arc.check_product()
-getwd()
-setwd("c:\\esri\\r-arcgis-tools")
-path_raster_entrada <- arc.open("cartagena.gdb\\cartagena")
+working_directory <- "C:\\esri\\r-arcgis-tools"
+setwd(working_directory)
+path_raster_entrada <- paste(working_directory,"\\cartagena.gdb\\cartagena",sep="")
 path_raster_img <- "c:\\esri\\raster_salida"
+
+
+
 
 #Leer raster de entrada
 obj_data <- arc.open(path_raster_entrada)
@@ -49,7 +52,12 @@ mi_raster <- as.raster(arc_raster)
 #viendo el metadato
 mi_raster
 ?raster
+ras <- stack(mi_raster)
 
+## Obtener el layer 2 individual
+banda_2 <- ras@layers[2][[1]] 
+
+hist(getValues(banda_2))
 hist(mi_raster[], main=NULL)
 
 dim(mi_raster)
@@ -75,22 +83,23 @@ plotRGB(mi_raster,r=7,g=6,b=5, stretch="hist")
 
 #Vegetación saludable	5 6 2
 
-
-#Tierra/agua	5 6 4
-
+plotRGB(mi_raster,r=5,g=6,b=2, stretch="hist")
+#Tiea/agua	5 6 4
+plotRGB(mi_raster,r=5,g=6,b=4, stretch="hist")
 #Natural con remoción atmosférica	7 5 3
-
+plotRGB(mi_raster,r=7,g=5,b=4, stretch="hist")
 #Infrarrojo de onda corta	7 5 4
-
+plotRGB(mi_raster,r=7,g=5,b=4, stretch="hist")
 
 #Análisis de vegetación	6 5 4
-
+plotRGB(mi_raster,r=6,g=5,b=4, stretch="hist")
 
 
 plotRGB(mi_raster,r=3,g=2,b=1, stretch="hist")
 
 
 plot(mi_raster)
+plot(banda_2)
 
 #kmean calssification
 ?kmeans
@@ -103,7 +112,7 @@ v <- na.omit(v)
 E <- kmeans(v,12,iter.max = 100, nstart = 10)
 kmeans_raster <- raster(mi_raster)
 kmeans_raster[i] <- E$cluster
-
+plot(kmeans_raster)
 writeRaster(kmeans_raster, path_raster_img, format="HFA")
 
 
